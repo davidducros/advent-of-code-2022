4253f2990661126bae50d9d07dbf8910829144e7
David Du Cros <dave@davidducros.com>
2022-12-06 10:35:55 +0000

    day3, part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,10 @@
-use std::{fs::File, io::{self, BufRead}, collections::HashSet};
+use std::{
+    collections::{HashSet, HashMap},
+    fs::File,
+    io::{self, BufRead}, string,
+};
 
+use itertools::Itertools;
 
 fn duplicate(s: &String) -> char {
     let mut items: HashSet<char> = HashSet::new();
@@ -12,7 +17,7 @@ fn duplicate(s: &String) -> char {
 
     for c in s.chars().rev().take(n) {
         if items.contains(&c) {
-            return c
+            return c;
         }
     }
 
@@ -27,16 +32,15 @@ fn score(c: char) -> u32 {
     let A = 'A' as u32;
 
     if v >= a && v <= z {
-        return v - a + 1; 
+        return v - a + 1;
     }
 
     return v - A + 27;
 }
 
 fn calculate(file: &str) -> u32 {
-
     let mut acc = 0;
-    
+
     let file = File::open(file).unwrap();
     for line in io::BufReader::new(file).lines() {
         if let Ok(line) = line {
@@ -47,11 +51,54 @@ fn calculate(file: &str) -> u32 {
     acc
 }
 
+fn common(group: &Vec<String>) -> char {
+
+    let mut items: HashMap<char, u32> = HashMap::new();
+
+    for bag in group {
+        for c in bag.chars().unique() {
+            *items.entry(c).or_insert_with(|| 0) += 1;
+        }
+    }
+
+    for entry in items {
+        if entry.1 == group.len() as u32 {
+            return entry.0;
+        }
+    }
+
+
+    panic!("no common items");
+}
+
+fn calculate_part2(file: &str, group_size: usize) -> u32 {
+    let mut acc = 0;
+
+    let file = File::open(file).unwrap();
+
+    let mut group: Vec<String> = vec![];
+
+    for line in io::BufReader::new(file).lines() {
+        if let Ok(line) = line {
+            group.push(line);
+
+            if group.len() == group_size {
+                acc += score(common(&group));
+                group.clear();
+            }
+        }
+    }
+
+    acc
+}
+
 fn main() {
     println!("result: {}", calculate("input/problem.txt"));
+    println!("result part 2: {}", calculate_part2("input/problem.txt", 3));
 }
 
 #[test]
 fn test_example() {
-    assert_eq!(157, calculate("input/example.txt"))
+    assert_eq!(157, calculate("input/example.txt"));
+    assert_eq!(70, calculate_part2("input/example.txt", 3));
 }
